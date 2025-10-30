50600262d356a0d79c4f00bff839d692ee2d9118
maracolman5691-svg <macbookmara@MacBook-Air-de-Mara.local>
2025-10-30 00:12:11 -0300

    Implementar la lógica de alimentación y presentación de la mascota virtual

--- Mascota_virtual_desafio.py
+++ Mascota_virtual_desafio.py
@@ -1,14 +1,39 @@
 # importar el modulo random
 import random
 # import imagen desde el archivo mascota.py
+from mascota import imagen
+
+
 
 
 class MascotaVirtual:
     def __init__(self, nombre):
-        pass
+        self.nombre = nombre
+        self.felicidad = 0
+        self.hambre = 0
+        self.imagen = imagen.inicio
+        self.imagen_triste = imagen.triste
+        self.imagen_feliz = imagen.feliz
+        self.imagen_muerto = imagen.muerto
+        self.imagen_disgustado = imagen.disgustado
+
 
     def alimentar(self):
-        pass
+        self.felicidad -= random.randint(5,10)
+        if self.felicidad < 0:
+            self.felicidad = 0
+        if self.hambre == 0: 
+            print(self.imagen_disgustado)
+            print(self.nombre, "Esta lleno.No puede comer más!")
+        else:
+            self.hambre -= random.randint(10, 15)
+            if self.hambre < 0:
+                self.hambre = 0
+            print (self.imagen_feliz)
+            print(self.nombre, "ha sido alimentado")
+
+    
+         
 
     def jugar(self):
         pass
@@ -17,10 +42,14 @@ class MascotaVirtual:
         pass
 
     def presentacion(self):  # opcional
-        pass
+        print(
+            f"\n╔════════════════════════════════════╗\n║     Te presento a tu mascota!      ║\n╚════════════════════════════════════╝\n{self.imagen}\tSu nombre es {self.nombre}"
+        )
 
     def despedida(self):  # opcional
-        pass
+                print(
+            f"\n╔════════════════════════════════════╗\n║             Nos vemos!             ║\n╚════════════════════════════════════╝{self.imagen}╔════════════════════════════════════╗\n║        Jueguemos otro día!         ║\n╚════════════════════════════════════╝\n"
+        )
 
 
 # Crear una instancia de MascotaVirtual
