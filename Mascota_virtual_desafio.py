84cbf6363f33ece802a05eb84c032639236a26f6
maracolman5691-svg <macbookmara@MacBook-Air-de-Mara.local>
2025-10-29 22:03:11 -0300

    Agregando los primeros archivos de la mascota virtual

---
+++ Mascota_virtual_desafio.py
@@ -0,0 +1,31 @@
+# importar el modulo random
+# import imagen desde el archivo mascota.py
+
+
+class MascotaVirtual:
+    def __init__(self, nombre):
+        pass
+
+    def alimentar(self):
+        pass
+
+    def jugar(self):
+        pass
+
+    def estado_animo(self):
+        pass
+
+    def presentacion(self):  # opcional
+        pass
+
+    def despedida(self):  # opcional
+        pass
+
+
+# Crear una instancia de MascotaVirtual
+
+# Interactuar con la mascota virtual
+# alimenta, juega y muestra su estado de animo
+# repite esta accion al menos 8 veces
+
+# Si te animas crea una interfaz para poder interactuar con tu mascota
